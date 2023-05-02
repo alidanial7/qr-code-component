68f3133c6b245b9981b8a73711fc08c9301fa499
Dani <danial@Danis-MacBook-Pro.local>
2023-05-03 01:54:22 +0330

    finish styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import qrCodeImage from "./assets/image-qr-code.png";
 
 function App() {
   return (
     <div className="w-screen h-screen bg-light-gray flex justify-center items-center">
-      <div className="bg-white p-10 rounded-xl">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="bg-white p-4 rounded-xl max-w-[300px]">
+        <img src={qrCodeImage} alt="qr-code" className="rounded-xl" />
+        <p className="font-bold text-xl mt-4 text-dark-blue text-center">
+          Improve your front-end skills by building projects
+        </p>
+        <p className="text-center text-grayish-blue mt-2">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
       </div>
     </div>
   );
