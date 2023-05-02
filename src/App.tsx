c52fdebf8cc70356f94b812776c12d5cb446415b
Dani <danial@Danis-MacBook-Pro.local>
2023-05-03 01:46:28 +0330

    add tailwind and add configs to it

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="w-screen h-screen bg-light-gray flex justify-center items-center">
+      <div className="bg-white p-10 rounded-xl">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </div>
   );
 }
 
